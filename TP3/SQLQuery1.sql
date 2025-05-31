be9b8da71448c56a89377072daba0486d29bb09c
Camila Goncalves <camiladenise.96@gmail.com>
2025-05-31 17:05:11 -0300

    tp4

---
+++ TP3/SQLQuery1.sql
@@ -0,0 +1,110 @@
+Use Trabajo_Practico_1
+
+--1-Todos los usuarios indicando Apellido, Nombre.
+SELECT Apellido, Nombre From Usuarios;
+
+--2-Todos los usuarios indicando Apellido y Nombre con el formato: [Apellido], [Nombre] (ordenados por Apellido en forma descendente). 
+SELECT Apellido + ', ' + Nombre AS 'Apellido y nombre' FROM Usuarios ORDER BY Apellido DESC;
+
+--3-Los usuarios cuyo IDTipoUsuario sea 5 (indicando Nombre, Apellido)
+SELECT Nombre + ', ' + Apellido AS 'Nombre y Apellido' FROM Usuarios U WHERE U.IdTipoUsuario = 5;
+
+--4-El último usuario del listado en orden alfabético (ordenado por Apellido y luego por Nombre). Indicar IDUsuario, Apellido y Nombre.
+SELECT TOP(1) IdUsuario, Apellido, Nombre FROM Usuarios ORDER BY Apellido DESC;
+
+--5-Los archivos cuyo año de creación haya sido 2021 (Indicar Nombre, Extensión y Fecha de creación).
+SELECT Nombre, Extension, FechaCreacion FROM Archivos WHERE YEAR(FechaCreacion) = 2021;
+
+--6-Todos los usuarios con el siguiente formato Apellido, Nombre en una nueva columna llamada ApellidoYNombre, en orden alfabético.
+SELECT Apellido + ', ' + Nombre AS 'ApellidoYNombre' FROM Usuarios ORDER BY Apellido ASC, Nombre ASC ;
+
+--7-Todos los archivos, indicando el semestre en el cual se produjo su fecha de creación. Indicar Nombre, Extensión, Fecha de Creación y la frase “Primer Semestre” o “Segundo Semestre” según corresponda.
+SELECT Nombre, Extension, FechaCreacion,
+CASE 
+	WHEN Month(FechaCreacion) <= 6 THEN 'Primer Semestre'
+	ELSE 'Segundo Semestre'
+END AS 'Semestre'
+FROM Archivos;
+
+--8-Ídem al punto anterior, pero ordenarlo por semestre y fecha de creación.
+SELECT Nombre, Extension, FechaCreacion,
+CASE 
+	WHEN Month(FechaCreacion) <= 6 THEN 'Primer Semestre'
+	ELSE 'Segundo Semestre'
+END AS 'Semestre'
+FROM Archivos ORDER BY Month(FechaCreacion);
+
+--9-Todas las Extensiones de los archivos creados. NOTA: no se pueden repetir.
+SELECT DISTINCT Extension FROM Archivos;
+
+--10-Todos los archivos que no estén eliminados. Indicar IDArchivo, IDUsuarioDueño, Fecha de Creación y Tamaño del archivo. Ordenar los resultados por Fecha de Creación (del más reciente al más antiguo).
+SELECT IdArchivo, IdUsuarioDuenio, FechaCreacion, Tamanio FROM Archivos WHERE Eliminado = 0 ORDER BY FechaCreacion DESC;
+
+--11-Todos los archivos que estén eliminados cuyo Tamaño del archivo se encuentre entre 40960 y 204800 (ambos inclusive). Indicar el valor de todas las columnas. 
+SELECT * FROM Archivos WHERE Eliminado = 1 AND Tamanio BETWEEN 40960 AND 204800;
+
+--12-Listar los meses del año en los que se crearon los archivos entre los años 2020 y 2022 (ambos inclusive). NOTA: no indicar más de una vez el mismo mes.
+SELECT DISTINCT MONTH(FechaCreacion) AS 'Meses' FROM Archivos WHERE YEAR(FechaCreacion) BETWEEN 2020 AND 2022
+
+--13-Indicar los distintos ID de los Usuarios Dueños de archivos que nunca modificaron sus archivos y que no se encuentren eliminados. NOTA: no se pueden repetir.
+SELECT DISTINCT IdUsuarioDuenio FROM Archivos WHERE FechaUltimaModificacion = FechaCreacion AND Eliminado = 0;
+
+--14-Listar todos los datos de los Archivos cuyos Dueños sean los usuarios con ID 1, 3, 5, 8, 9. Los registros deben estar ordenados por IDUsuarioDueño y Tamaño de forma ascendente.
+SELECT * FROM Archivos WHERE IdUsuarioDuenio IN (1,3,5,8,9) ORDER BY IdUsuarioDuenio ASC, Tamanio ASC;
+
+--15-Listar todos los datos de los tres Archivos de más bajo Tamaño que no se encuentren Eliminados.
+SELECT TOP(3) * FROM Archivos WHERE Eliminado = 0 ORDER BY Tamanio ASC;
+
+--16-Listar los Archivos que estén Eliminados y la Fecha de Ultima Modificación sea menor o igual al año 2021 o bien no estén Eliminados y la Fecha de Ultima Modificación sean mayor al año 2021. Indicar todas las columnas excepto IDUsuarioDueño y Fecha de Creación. Ordenar por IDArchivo.
+SELECT IdArchivo, Nombre, Extension, Descripcion, IDTipoArchivo, Tamanio, FechaUltimaModificacion, Eliminado 
+FROM Archivos 
+WHERE (Eliminado = 1 AND YEAR(FechaUltimaModificacion) <= 2021) OR (Eliminado = 0 AND YEAR(FechaUltimaModificacion) > 2021) 
+ORDER BY IdArchivo ASC;
+
+--17-Listar los Archivos creados en el año 2023 indicando todas las columnas y además una llamada “DiaSemana” que devuelva a qué día de la semana (1-7) corresponde la fecha de creación del archivo. Ordenar los registros por la columna que contiene el día de la semana.
+--DESAFÍO: crear otra columna llamada DiaSemanaEnLetras que contenga el día de la semana, pero en letras (suponiendo que la semana comienza en 1-DOMINGO). Por ejemplo, si la fecha del Archivo es 20/08/2023, la columna DiaSemana debe contener 1 y la columna DiaSemanaEnLetras debe contener DOMINGO.
+SELECT *,
+CASE
+	WHEN DAY(FechaCreacion) IN (1,8,15,22,29) THEN 1
+	WHEN DAY(FechaCreacion) IN (2,9,16,23,30) THEN 2
+	WHEN DAY(FechaCreacion) IN (3,10,17,24,31) THEN 3
+	WHEN DAY(FechaCreacion) IN (4,11,18,25) THEN 4
+	WHEN DAY(FechaCreacion) IN (5,12,19,26) THEN 5
+	WHEN DAY(FechaCreacion) IN (6,13,20,27) THEN 6
+	ELSE 7
+END AS 'DiaSemana',
+CASE 
+	WHEN DAY(FechaCreacion) IN (1,8,15,22,29) THEN 'Domingo'
+	WHEN DAY(FechaCreacion) IN (2,9,16,23,30) THEN 'Lunes'
+	WHEN DAY(FechaCreacion) IN (3,10,17,24,31) THEN 'Martes'
+	WHEN DAY(FechaCreacion) IN (4,11,18,25) THEN 'Miercoles'
+	WHEN DAY(FechaCreacion) IN (5,12,19,26) THEN 'Jueves'
+	WHEN DAY(FechaCreacion) IN (6,13,20,27) THEN 'Viernes'
+	ELSE 'Sabado'
+END AS 'DiaSemanaEnLetras'
+FROM Archivos WHERE YEAR(FechaCreacion) = 2023
+ORDER BY DiaSemana ASC;
+
+--18-Listar los Archivos que no estén Eliminados y cuyo mes de creación coincida con el mes actual (sin importar el año). NOTA: obtener el mes actual mediante una función, no forzar el valor.
+SELECT * FROM Archivos WHERE Eliminado = 0 AND MONTH(FechaCreacion) = MONTH(GETDATE());
+
+--19-Listar la cantidad total de archivos creados en cada año, mostrando una columna con el año (obtenido a partir de la fecha de creación) y otra con la cantidad de archivos correspondientes a ese año. Además, si se animan, como desafío adicional, agreguen a la consulta una tercera columna llamada PorcentajeSobreElTotal, que indique qué porcentaje representa dicha cantidad sobre el total general de archivos registrados en la base. El porcentaje debe mostrarse con dos decimales y calcularse sin utilizar subconsultas. Ordenar el resultado por año.
+SELECT 
+  YEAR(FechaCreacion) AS Anio,
+  COUNT(*) AS Cantidad,
+  ROUND(100.0 * COUNT(*) / SUM(COUNT(*)) OVER (), 2) AS PorcentajeSobreElTotal
+FROM Archivos
+GROUP BY YEAR(FechaCreacion)
+ORDER BY Anio;
+
+--20-Listar los Archivos que no hayan sido creados por los Usuarios con ID 1, 2, ,5, 9 y 10. Indicar IDUsuarioDueño, IDArchivo, fecha de creación y Tamaño. Ordenar por IDUsuarioDueño.
+SELECT IdUsuarioDuenio, IdArchivo, FechaCreacion, Tamanio FROM Archivos WHERE IdUsuarioDuenio NOT IN (1,2,5,9,10);
+
+--21-Listar todos los datos de los Usuarios cuyos apellidos comienzan con J. Hacer la misma consulta para los Usuarios con apellido que comienza con J y el Nombre comienza con E. Ordenar los registros por IDUsuario.
+SELECT * FROM Usuarios WHERE Apellido LIKE 'J%';
+SELECT * FROM Usuarios WHERE Apellido LIKE 'J%' AND Nombre Like 'E%'
+ORDER BY IdUsuario;
+
+--22-Listar todos los datos de los Archivos que tengan el mayor Tamaño. En caso de empate se deben listar todos los Archivos con igual Tamaño.
+SELECT TOP(10) * FROM Archivos ORDER BY Tamanio DESC;
+
---
+++ TP4/SQLQuery1.sql
@@ -0,0 +1,136 @@
+USE Trabajo_Practico_1
+--1-Por cada usuario listar el nombre, apellido y el nombre del tipo de usuario.
+SELECT U.Nombre, U.Apellido, TU.TipoUsuario
+FROM Usuarios U 
+LEFT JOIN  TiposUsuarios AS TU 
+ON U.IdTipoUsuario = U.IdTipoUsuario;
+
+--2-Listar el ID, nombre, apellido y tipo de usuario de aquellos usuarios que sean del tipo 'Suscripción Free' o 'Suscripción Básica'
+SELECT  *
+FROM Usuarios U
+INNER JOIN TiposUsuarios AS TU ON U.IdUsuario = TU.IdTipoUsuario WHERE TU.IdTipoUsuario = 1 OR TU.IdTipoUsuario = 2;
+
+--3-Listar los nombres de archivos, extensión, tamaño expresado en Megabytes y el nombre del tipo de archivo.
+--NOTA: En la tabla Archivos el tamaño está expresado en Bytes.
+SELECT A.Nombre, A.Extension, Tamanio, TA.TipoArchivo 
+FROM Archivos A
+INNER JOIN TiposArchivos AS TA ON TA.IdTipoArchivo = A.IDTipoArchivo;
+
+--4-Listar los nombres de archivos junto a la extensión con el siguiente formato 'NombreArchivo.extension'. Por ejemplo, 'Actividad.pdf'.
+--Sólo listar aquellos cuyo tipo de archivo contenga los términos 'ZIP', 'Word', 'Excel', 'Javascript' o 'GIF'
+SELECT A.Nombre + '.' + A.Extension AS 'NombreArchivo.extension'
+FROM Archivos A
+RIGHT JOIN TiposArchivos AS TA ON TA.IdTipoArchivo = A.IDTipoArchivo
+WHERE TA.TipoArchivo LIKE '%ZIP%' OR TA.TipoArchivo Like '%Word%' OR TA.TipoArchivo Like'%Excel%' OR TA.TipoArchivo LIKE'%Javascript%'
+OR TA.TipoArchivo Like'%GIF%';
+
+--5-Listar los nombres de archivos, su extensión, el tamaño en megabytes y la fecha de creación de aquellos archivos que le pertenezcan al usuario dueño con nombre 'Michael' y apellido 'Williams'.
+SELECT A.Nombre, A.Extension, A.Tamanio, A.FechaCreacion
+FROM Archivos A
+INNER JOIN Usuarios AS U ON U.IdUsuario = A.IdUsuarioDuenio
+WHERE U.Nombre Like 'Michael' AND U.Apellido Like 'Williams';
+
+--6-Listar los datos completos del archivo más pesado del usuario dueño con nombre 'Michael' y apellido 'Williams'. Si hay varios archivos que cumplen esa condición, listarlos a todos.
+SELECT *
+FROM Archivos A
+INNER JOIN Usuarios AS U ON U.IdUsuario = A.IdUsuarioDuenio
+WHERE U.Nombre Like 'Michael' AND U.Apellido Like 'Williams';
+
+--7-Listar nombres de archivos, extensión, tamaño en bytes, fecha de creación y de modificación, apellido y nombre del usuario dueño de aquellos archivos cuya descripción contengan el término 'empresa' o 'presupuesto'
+SELECT A.Nombre, A.Extension, A.Tamanio, A.FechaCreacion, A.FechaUltimaModificacion, U.Apellido, U.Nombre
+FROM Archivos A
+INNER JOIN Usuarios U ON U.IdUsuario = A.IdUsuarioDuenio
+WHERE A.Descripcion Like '%empresa%' OR A.Descripcion Like 'presupuesto';
+
+--8-Listar las extensiones sin repetir de los archivos cuyos usuarios dueños tengan tipo de usuario 'Suscripción Plus', 'Suscripción Premium' o 'Suscripción Empresarial'
+SELECT DISTINCT A.Extension
+FROM Archivos A
+INNER JOIN Usuarios U ON U.IdUsuario = A.IdUsuarioDuenio
+INNER JOIN TiposUsuarios TU ON TU.IdTipoUsuario = U.IdTipoUsuario
+WHERE TU.TipoUsuario Like 'Suscripción Plus' OR TU.TipoUsuario Like 'Suscripción Premium' OR TU.TipoUsuario Like 'Suscripción Empresarial';
+
+--9-Listar los apellidos y nombres de los usuarios dueños y el tamaño del archivo de los tres archivos con extensión 'zip' más pesados. Puede ocurrir que el mismo usuario aparezca varias veces en el listado.
+SELECT U.Apellido, U.Nombre, A.Tamanio
+FROM Archivos A
+INNER JOIN Usuarios U ON U.IdUsuario = A.IdUsuarioDuenio
+WHERE A.Extension Like '%zip%'
+ORDER BY A.Tamanio DESC;
+
+--10-Por cada archivo listar el nombre del archivo, la extensión, el tamaño en bytes, el nombre del tipo de archivo y el tamaño calculado en su mayor expresión y la unidad calculada. 
+--Siendo Gigabytes si al menos pesa un gigabyte, Megabytes si al menos pesa un megabyte, Kilobyte si al menos pesa un kilobyte o en su defecto expresado en bytes.
+--Por ejemplo, si el archivo imagen.jpg pesa 40960 bytes entonces debe figurar 40 en la columna Tamaño Calculado y 'Kilobytes' en la columna unidad.
+SELECT A.Nombre, A.Extension, A.Tamanio, TA.TipoArchivo
+FROM Archivos A
+INNER JOIN TiposArchivos TA ON TA.IdTipoArchivo = A.IDTipoArchivo; --TERMINAR...
+
+--11-Listar los nombres de archivo y extensión de los archivos que han sido compartidos.
+SELECT DISTINCT A.Nombre, A.Extension
+FROM ArchivosCompartidos AC
+INNER JOIN Usuarios U ON U.IdUsuario = U.IdUsuario
+INNER JOIN Archivos A ON A.IDTipoArchivo = AC.IdArchivo
+WHERE FechaCompartido IS NOT NULL;
+
+--12-Listar los nombres de archivo y extensión de los archivos que han sido compartidos a usuarios con apellido 'Clarck' o 'Jones'
+SELECT A.Nombre, A.Extension
+FROM Archivos A
+INNER JOIN Usuarios U ON U.IdUsuario = A.IdUsuarioDuenio
+INNER JOIN ArchivosCompartidos AC ON AC.IdArchivo = A.IdArchivo
+WHERE AC.FechaCompartido IS NOT NULL AND (U.Apellido Like 'Clarck' OR U.Apellido Like 'Jones');
+
+--13-Listar los nombres de archivo, extensión, apellidos y nombres de los usuarios a quienes se le hayan compartido archivos con permiso de 'Escritura'
+SELECT A.Nombre, A.Extension, U.Apellido, U.Nombre
+FROM Archivos A
+INNER JOIN Usuarios U ON U.IdUsuario = A.IdUsuarioDuenio
+INNER JOIN ArchivosCompartidos AC ON AC.IdArchivo = A.IdArchivo
+INNER JOIN Permisos P ON P.IdPermiso = AC.IdPermiso
+WHERE P.Nombre Like 'Escritura' AND AC.FechaCompartido IS NOT NULL;
+
+--14-Listar los nombres de archivos y extensión de los archivos que no han sido compartidos.
+SELECT A.Nombre, A.Extension
+FROM Archivos A
+WHERE NOT IN (SELECT IdArchivo FROM Archivos Ar
+INNER JOIN ArchivosCompartidos AC ON Ar.IdArchivo = AC.IdArchivo); --REVISAR
+
+--15-Listar los apellidos y nombres de los usuarios dueños que tienen archivos eliminados.
+SELECT U.Apellido, U.Nombre FROM Archivos A
+INNER JOIN Usuarios U ON U.IdUsuario= A.IdUsuarioDuenio
+WHERE A.Eliminado = 1;
+
+--16-Listar los nombres de los tipos de suscripciones, sin repetir, que tienen archivos que pesan al menos 120 Megabytes.
+SELECT TU.TipoUsuario, A.Tamanio
+FROM Archivos A
+INNER JOIN Usuarios U ON U.IdUsuario = A.IdUsuarioDuenio
+INNER JOIN TiposUsuarios TU ON TU.IdTipoUsuario = U.IdTipoUsuario
+WHERE A.Tamanio >= 120; --REVISAR CREO QUE TENGO QUE CONVERTIR TAMANIO A MEGABYTES Y AHI COMPARAR CONTRA 120.
+
+--17-Listar los apellidos y nombres de los usuarios dueños, nombre del archivo, extensión, fecha de creación, fecha de modificación y la cantidad de días transcurridos desde la última modificación.
+--Sólo incluir a los archivos que se hayan modificado (fecha de modificación distinta a la fecha de creación).
+SELECT U.Apellido, U.Nombre, A.Nombre, A.Extension, A.FechaCreacion, A.FechaUltimaModificacion, DATEDIFF(DAY, A.FechaCreacion, A.FechaUltimaModificacion) AS 'Cant dias transcurridos' FROM Archivos A
+INNER JOIN Usuarios U ON U.IdUsuario = A.IdUsuarioDuenio
+WHERE A.FechaUltimaModificacion > A.FechaCreacion;
+
+--18-Listar nombres de archivos, extensión, tamaño, apellido y nombre del usuario dueño del archivo, apellido y nombre del usuario que tiene el archivo compartido y el nombre de permiso otorgado.
+SELECT A.Nombre, A.Extension, A.Extension, U.Apellido, U.Nombre
+FROM Archivos A
+INNER JOIN Usuarios U ON U.IdUsuario = A.IdUsuarioDuenio
+INNER JOIN ArchivosCompartidos AC ON AC.IdArchivo = A.IdArchivo;--TODO: TERMINAR, CREO QUE HAY QUE USAR SUBCONSULTAS.
+
+--19-Listar nombres de archivos, extensión, tamaño, apellido y nombre del usuario dueño del archivo, apellido y nombre del usuario que tiene el archivo compartido y el nombre de permiso otorgado.
+--Sólo listar aquellos registros cuyos tipos de usuarios coincidan tanto para el dueño como para el usuario al que le comparten el archivo.
+SELECT A.Nombre, A.Extension, A.Extension, U.Apellido, U.Nombre
+FROM Archivos A
+INNER JOIN Usuarios U ON U.IdUsuario = A.IdUsuarioDuenio
+INNER JOIN ArchivosCompartidos AC ON AC.IdArchivo = A.IdArchivo;--TODO:TERMINAR..
+
+
+--20-Apellido y nombre de los usuarios que tengan compartido o sean dueños del archivo con nombre 'Documento Legal'.
+SELECT DISTINCT U.Apellido, U.Nombre
+FROM Archivos A
+LEFT JOIN ArchivosCompartidos AC ON AC.IdArchivo = A.IdArchivo
+LEFT JOIN Usuarios U ON U.IdUsuario = AC.IdUsuario
+WHERE A.Nombre = 'Documento Legal'
+UNION
+SELECT U.Apellido, U.Nombre
+FROM Archivos A
+JOIN Usuarios U ON U.IdUsuario = A.IdUsuarioDuenio
+WHERE A.Nombre = 'Documento Legal'; 
